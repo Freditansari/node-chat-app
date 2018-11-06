a3622424d0ac0dfcbf98f9c34fa8d301b5da92c2
ftansari <freditansari@gmail.com>
2018-11-06 15:14:47 +0700

    added connected and disconnected to server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,40 @@
 //nodejs path
 const path = require('path');
 const express = require('express');
+const socketio = require('socket.io');
+
+//requried to serve socket
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 
+//needed for socket
+var server = http.createServer(app);
+var io= socketio(server);
+
 //to serve public path html pages
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+/**this shows a connection on server
+ * the event is built in from socket.io
+ */
+io.on('connection', (socket)=>{
+    console.log('new user connected');
+    
+    socket.on('disconnect', (socket)=>{
+        console.log('a client disconnected from our server')
+    });
+});
+
+//need to find the proper event
+io.on('disconnect', (socket)=>{
+    console.log('a user disconnected');
+});
+// app.listen(port, () => {
+//   console.log(`Server is up on ${port}`);
+// });
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
