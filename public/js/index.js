1d7db2af7f53687f1d7f86658a4129ba1d65debc
ftansari <freditansari@gmail.com>
2018-11-08 12:39:31 +0700

    added chat system

---
+++ public/js/index.js
@@ -0,0 +1,63 @@
+
+var socket = io();
+socket.on('connect', function () {
+
+
+    socket.emit('newUserLoggedIn', {
+        userName: Math.random().toString(36).substr(2, 9)
+    });
+    // socket.emit('createMessage',
+    // {
+    //     from: 'frank', 
+    //     message: 'Hi'
+    // },
+    // function (data) {
+    //     console.log('got it ', data);
+    // });
+
+//   socket.emit('createEmail',{
+//       to: 'jen@example.vom',
+//       text: 'hey this is fredy'
+//   });
+//   socket.emit('createMessage', {
+//       "from":"client@gmail.com",
+//       "message":"Hello there, I need a refund on my purchase of how to do socket.io book"
+//     });
+})
+
+socket.on('disconnect',function (){
+  console.log('a client is disconnected from server');
+});
+
+
+
+//listener
+// socket.on('newEmail', function(email){
+//     console.log('new email',email);
+//   });
+
+socket.on('newMessage', function(receivedMessage){
+    console.log(receivedMessage);
+    var li = jQuery('<li></li>');
+    li.text(`${receivedMessage.from}: ${receivedMessage.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+socket.on('welcomeMessage', function(receivedWelcomeMessage){
+    console.log(receivedWelcomeMessage);
+});
+
+jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+    socket.emit('createMessage',
+    {
+        from: 'User', 
+        text: jQuery('[name=message').val()
+    },
+    function (data) {
+        console.log('got it ', data);
+    });
+
+});
+
